fc06879a00f3ff30734430555ffdb33c9a8d1f22
Mike Smith <smith.mike@mac.com>
2019-10-19 19:34:12 -0400

    upadted server.js

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -4,7 +4,7 @@ import "./style.css";
 const Footer = () => (
 	<footer className = "page-footer">
 		<div className = "container-fluid text-center">
-			<p>Duncan Hieu M. Dam</p>
+			<p>Mike Q Smith</p>
 		</div>	
 	</footer>
 );
